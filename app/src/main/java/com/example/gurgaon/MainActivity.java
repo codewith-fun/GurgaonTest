3ae3a4b4b62ca54ad9cb3d855b69cee4d9dcc97e
codewith-fun <laxmipalkant01@gmail.com>
2021-10-27 12:59:18 +0530

    added location access

--- app/src/main/java/com/example/gurgaon/MainActivity.java
+++ app/src/main/java/com/example/gurgaon/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.gurgaon;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gurgaon.model.UserModel;
 import com.example.gurgaon.repository.UserInfoDao;
@@ -17,7 +31,7 @@ import com.example.gurgaon.viewmodel.UserViewModel;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
 
     private UserViewModel viewModel;
@@ -26,18 +40,44 @@ public class MainActivity extends AppCompatActivity {
     EditText mob,name;
     TextView prompt;
     UserInfoDao userInfoDao;
+
+    /*
+    *
+    * */
+    private static final int REQUEST_LOCATION = 1;
+    Button btnGetLocation;
+    TextView showLocation;
+    LocationManager locationManager;
+    String latitude, longitude;
+    TextView txtLat;
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions( this,
+                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
 
         viewModel = ViewModelProviders.of(MainActivity.this).get(UserViewModel.class);
         initView();
         actionWithData();
-
+        doStuff();
 
     }
 
+    private void doStuff() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            OnGPS();
+        } else {
+            getLocation();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void actionWithData() {
         progressDialog = ProgressDialog.show(MainActivity.this,"Loading....","Please wait",true);
         viewModel.getUAllInfo().observe(this,userModels -> {
@@ -48,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void ObserveData(List<UserModel> data) {
 //        Log.d("TAG", "users: "+data.toString());
         button.setOnClickListener(view -> {
@@ -69,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
                             userModel.setName(userName);
                             prompt.setVisibility(View.GONE);
                             viewModel.insert(userModel);
-                            Log.d("TAG", "ObserveData: added ");
+                            Log.d("TAG", "ObserveData: added "+data.stream().filter(userModel1 -> mobile.equals(user.getMob())));
 
                         }
                     }
@@ -97,5 +138,45 @@ public class MainActivity extends AppCompatActivity {
         mob = findViewById(R.id.mob);
         name = findViewById(R.id.name);
         prompt = findViewById(R.id.msg_prompt);
+        txtLat = findViewById(R.id.msg_latlang);
+    }
+
+
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+                showLocation.setText("Your Location: " + "\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+            } else {
+                Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
